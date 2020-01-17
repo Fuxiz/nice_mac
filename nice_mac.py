7a12a8b1d4083b1480567c215fd4e3e1854141b0
Fuxiz <file-stef@hotmail.com>
2020-01-17 22:37:39 +0100

    Add files via upload

---
+++ nice_mac.py
@@ -0,0 +1,17 @@
+mac2 = "d0509954c1d4"
+mac2 = mac2.replace('.','').replace(':','').replace('-','')
+charmac = [] 
+for char in mac2:
+    charmac.append(str(char)) 
+macindex = charmac.copy()
+l = [[":","-","."],[2, 5, 8, 11, 14],[4,9],[2, 5, 8, 11, 14]]
+for g in range(0,len(l)-1):     #Loops through every list
+    m = g
+    g = g+1                     # Jumps to third list to use the amount of space
+    for i in range(0,len(l[g])):       #looks up and loops the amount of character in the list 
+        charmac.insert(l[g][i], l[0][m])  #inserts all the character in the list and in the the position of the lists. With power of 0 i always stay at the characters
+    print("".join(charmac))     
+    charmac = macindex.copy()   # Clears for new characters
+print("".join(charmac))     #Makes a list looks shiny 
+macindex.insert(6,"-")
+print("".join(macindex))
