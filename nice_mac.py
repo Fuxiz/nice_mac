e62ac1b38ff2ea5fa9019d73d32e898a06a1724e
Fuxiz <file-stef@hotmail.com>
2020-01-18 13:29:56 +0100

    Update nice_mac.py

--- nice_mac.py
+++ nice_mac.py
@@ -1,18 +1,13 @@
 import sys
-mac = str(sys.argv[1])
-mac = mac.replace('.','').replace(':','').replace('-','')
-charmac = [] 
-for char in mac:
-    charmac.append(str(char)) 
-macindex = charmac.copy()
-l = [[":","-","."],[2, 5, 8, 11, 14],[4,9],[2, 5, 8, 11, 14]]
+mac2 = str(sys.argv[1]).replace('.','').replace(':','').replace('-','')
+macindex = [str(char) for char in mac2] 
+cleanmac = macindex.copy()
+l = [[":","-","-",".","."],[2, 5, 8, 11, 14],[6],[4,9],[2, 5, 8, 11, 14],[4,9]]
 for g in range(0,len(l)-1):     #Loops through every list
     m = g
-    g = g+1                     # Jumps to third list to use the amount of space
+    g = g+1                     # Jumps to second list for the positions
     for i in range(0,len(l[g])):       #looks up and loops the amount of character in the list 
-        charmac.insert(l[g][i], l[0][m])  #inserts all the character in the list and in the the position of the lists. With power of 0 i always stay at the characters
-    print("".join(charmac))     
-    charmac = macindex.copy()   # Clears for new characters
-print("".join(charmac))     #Makes a list looks shiny 
-macindex.insert(6,"-")
-print("".join(macindex))
+        macindex.insert(l[g][i], l[0][m])  #inserts all the character in the list and in the the position of the lists.
+    print("".join(macindex))     
+    macindex = cleanmac.copy()   # Clears for new characters
+print("".join(macindex))     #Makes a list looks shiny 
