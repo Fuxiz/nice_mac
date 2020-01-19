16a9d8d679794a3dd1983ba9e88b684ea870afcc
Fuxiz <file-stef@hotmail.com>
2020-01-19 17:10:50 +0100

    Update nice_mac.py

--- nice_mac.py
+++ nice_mac.py
@@ -1,13 +1,12 @@
 import sys
-mac2 = str(sys.argv[1]).replace('.','').replace(':','').replace('-','')
-macindex = [str(char) for char in mac2] 
-cleanmac = macindex.copy()
+# mac2 = str(sys.argv[1]).replace('.','').replace(':','').replace('-','')
+mac2 = list("32-15-D2-36-1D-52 ".replace('.','').replace(':','').replace('-',''))
+cleanmac = mac2.copy()
 l = [[":","-","-",".","."],[2, 5, 8, 11, 14],[6],[4,9],[2, 5, 8, 11, 14],[4,9]]
 for g in range(0,len(l)-1):     #Loops through every list
-    m = g
-    g = g+1                     # Jumps to second list for the positions
+    m = g; g+=1                       #while g steps through each list it also steps through each character
     for i in range(0,len(l[g])):       #looks up and loops the amount of character in the list 
-        macindex.insert(l[g][i], l[0][m])  #inserts all the character in the list and in the the position of the lists.
-    print("".join(macindex))     
-    macindex = cleanmac.copy()   # Clears for new characters
-print("".join(macindex))     #Makes a list looks shiny 
+        mac2.insert(l[g][i], l[0][m])  #inserts all the character in the list and in the the position of the lists.
+    print("".join(mac2))     
+    mac2 = cleanmac.copy()   # Clears for new characters
+print("".join(mac2))     #Makes a list looks shiny 
