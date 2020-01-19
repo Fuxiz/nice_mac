ff093cc88e27c35b0d5d8a8312e80cb7fe9c1d98
Fuxiz <file-stef@hotmail.com>
2020-01-19 17:53:06 +0100

    Update nice_mac.py

--- nice_mac.py
+++ nice_mac.py
@@ -1,6 +1,6 @@
 import sys
 mac2 = list(str(sys.argv[1]).replace('.','').replace(':','').replace('-',''))
-cleanmac = mac2.copy()
+cleanmac = mac2.copy();print("".join(cleanmac))
 l = [[":","-","-",".","."],[2, 5, 8, 11, 14],[6],[4,9],[2, 5, 8, 11, 14],[4,9]]
 for g in range(0,len(l)-1):     #Loops through every list
     m = g; g+=1                       #while g steps through each list it also steps through each character
@@ -8,4 +8,3 @@ for g in range(0,len(l)-1):     #Loops through every list
         mac2.insert(l[g][i], l[0][m])  #inserts all the character in the list and in the the position of the lists.
     print("".join(mac2))     
     mac2 = cleanmac.copy()   # Clears for new characters
-print("".join(mac2))     #Makes a list looks shiny 
