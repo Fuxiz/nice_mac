d702ad7c87cc1c2622dc73389582a98232ef721f
Fuxiz <file-stef@hotmail.com>
2020-09-28 17:33:38 +0200

    Update PFfilter.py

--- PFrulefilter/PFfilter.py
+++ PFrulefilter/PFfilter.py
@@ -1,21 +1,29 @@
-
-
 syntaxlist = [['pass','block'],['in','out'],['log'],['quick'],['egress','vmx0'],['inet','inet6'],['tcp','udp','icmp','icmp6','http','https'],['from'],['to'],['port'],['flags S/SA'],['no state','keep state','modulate state','synproxy state']]
 
 
 with open('pfrule.txt','r') as f:
     for line in f:
-        rule = line.split()
-        rulelen = len(rule)
-        for s in range(len(rule)):
+        rules = line.split()
+        rulelen = len(rules)
+        macrohost = ""
+        isList = False
+        filteredRules = []
+        for rule in rules:
+            if rule == "{":
+                isList = True
+            elif str(rule) == "}":
+                isList = False
+                if rule[0] == "$":
+                    macrohost += rule
+            else:
+                macrohost += rule
+            if not isList:
+                filteredRules.append(macrohost)
             for rulecount in range(len(syntaxlist)):
                 if rulecount == 7:
-                    
-                    print("source ip is", rule[s])
+                    print("source ip is", rule[rule])
                     rulecount += 1
-                    break
                 for listitem in range(len(syntaxlist[rulecount])):
-                    if rule[s] == syntaxlist[rulecount][listitem]:
-                        print(rule[s])
-        print("\n")
-
+                    if rules[rule] == syntaxlist[rulecount][listitem]:
+                        print(rules[rule])
+    print("\n")
