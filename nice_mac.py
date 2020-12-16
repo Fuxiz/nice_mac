de46d54a845ba9a43ee3eb66d9e7b456f680e03a
Fuxiz <file-stef@hotmail.com>
2020-12-16 20:12:08 +0100

    Feature to read multiple arguments

--- nice_mac.py
+++ nice_mac.py
@@ -1,10 +1,14 @@
 import sys
-mac2 = list(str(sys.argv[1]).replace('.','').replace(':','').replace('-',''))
-cleanmac = mac2.copy();print("".join(cleanmac))
-l = [[":","-","-",".","."],[2, 5, 8, 11, 14],[6],[4,9],[2, 5, 8, 11, 14],[4,9]]
-for g in range(0,len(l)-1):     #Loops through every list
-    m = g; g+=1                       #while g steps through each list it also steps through each character
-    for i in range(0,len(l[g])):       #looks up and loops the amount of character in the list 
-        mac2.insert(l[g][i], l[0][m])  #inserts all the character in the list and in the the position of the lists.
-    print("".join(mac2))     
-    mac2 = cleanmac.copy()   # Clears for new characters
+
+syslen= len(sys.argv)
+for numb in range(1, syslen):
+    mac2 = list(str(sys.argv[numb]).replace('.','').replace(':','').replace('-',''))
+    cleanmac = mac2.copy();print("".join(cleanmac))
+    l = [[":","-","-",".","."],[2, 5, 8, 11, 14],[6],[4,9],[2, 5, 8, 11, 14],[4,9]]
+    for g in range(0,len(l)-1):     #Loops through every list
+        m = g; g+=1                       #while g steps through each list it also steps through each character
+        for i in range(0,len(l[g])):       #looks up and loops the amount of character in the list 
+            mac2.insert(l[g][i], l[0][m])  #inserts all the character in the list and in the the position of the lists.
+        print("".join(mac2))     
+        mac2 = cleanmac.copy()   # Clears for new characters
+    print('\n')
